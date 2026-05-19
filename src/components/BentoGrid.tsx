b3b0c8c56e2bf74563d801cc196b029c68d778b0
exyzte <davidecicerys@gmail.com>
2026-05-19 15:40:45 -0500

    aa

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -23,15 +23,6 @@ const PROJECTS = [
   },
   {
     id: "project-2",
-    title: "Byron's Landscaping | Dynamic Portfolio",
-    description: "A premium landscaping platform featuring a dynamically rendered GSAP portfolio. Allows administrators to seamlessly replace showcased images and text from a secure, custom-built admin dashboard.",
-    tech: ["React", "GSAP Animations", "Next.js", "Admin Dashboard", "Dynamic Content"],
-    type: "featured",
-    image: "/images/byrons.png",
-    link: "https://byron-s-landscaping.vercel.app/"
-  },
-  {
-    id: "project-3",
     title: "Embody Ink | Studio Management System",
     description: "A high-end platform for a tattoo studio. Includes an interactive calendar scheduling tool, completely manageable from an authenticated, Apple-inspired admin dashboard.",
     tech: ["Next.js", "Scheduling API", "Admin Dashboard", "Full Stack"],
@@ -40,7 +31,7 @@ const PROJECTS = [
     link: "https://embodyink.vercel.app"
   },
   {
-    id: "project-4",
+    id: "project-3",
     title: "AI-Restaurant Ecosystem - Domo Burguer",
     description: "A high-end, MERN stack burger delivery platform featuring an AI Agent assistant, Leaflet integration, Order Processing and more.",
     tech: ["MERN", "AI Agent", "Leaflet", "Map Customization"],
@@ -49,14 +40,23 @@ const PROJECTS = [
     link: "https://full-stack-smart-restaurant-ecosystem-davids-projects-b21f3f2a.vercel.app/"
   },
   {
-    id: "project-5",
+    id: "project-4",
     title: "Music Record Label Environment",
     description: "An immersive app for a record label. Showcasing releases, news, artists, albums, and interactive media. Railway hosted backend with MongoDB and a React frontend. Built with a focus on sleek design, GSAP animations, and responsive design.",
     tech: ["React Native", "Express", "TypeScript", "Node", "MongoDB - Supabase", "GSAP Animations", "Responsive Design", "Railway Hosting"],
     type: "featured",
     image: "/images/wayra.png",
     link: "https://wayra.space"
-  }
+  },
+  {/* {
+    id: "project-2",
+    title: "Byron's Landscaping | Dynamic Portfolio",
+    description: "A premium landscaping platform featuring a dynamically rendered GSAP portfolio. Allows administrators to seamlessly replace showcased images and text from a secure, custom-built admin dashboard.",
+    tech: ["React", "GSAP Animations", "Next.js", "Admin Dashboard", "Dynamic Content"],
+    type: "featured",
+    image: "/images/byrons.png",
+    link: "https://byron-s-landscaping.vercel.app/"
+  }, */}
 ];
 
 export default function BentoGrid() {
